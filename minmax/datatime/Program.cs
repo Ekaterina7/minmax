08824d7f58c9a90c6c6e7e9fd650706ce8895c58
Ekaterina7 <mmmkatia@bk.ru>
2017-05-06 21:28:48 +0300

    addnew

---
+++ minmax/datatime/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace datatime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = System.IO.File.ReadAllText(@"C:\Users\Екатерина\Desktop\катя.txt");
+
+
+            string[] textstolb = text.Split(';');
+            DateTime min = DateTime.MaxValue;
+            int it = 0;
+            for (int i = 0; i < textstolb.Length - 1; i += 6)
+            {
+
+                DateTime tmp = Convert.ToDateTime(Convert.ToString(textstolb[i + 4]));
+                if (tmp < min)
+                {
+                    min = tmp;
+                    it = i;
+                }
+
+
+
+            }
+            Console.WriteLine("Самый старший "+ textstolb[it] + " " + textstolb[it + 1] + " " + textstolb[it + 2] + " " + textstolb[it + 3] + " " + textstolb[it + 4] + " " + textstolb[it + 5]);
+
+
+
+        }
+    }
+}
