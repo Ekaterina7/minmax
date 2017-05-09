abc3e312fe09410bc57e1026ad91620277638eff
Ekaterina7 <mmmkatia@bk.ru>
2017-05-09 07:47:36 +0300

    addmidleleveltask12

---
+++ minmax/midlelevel12/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace midlelevel12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = System.IO.File.ReadAllText(@"C:\Users\Екатерина\Desktop\средний.txt");
+            Console.WriteLine(text);
+            string number = "1234567890";
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < number.Length; j++)
+                {
+                    if (text[i]==number[j])
+                    {
+                        Console.WriteLine(text[i] + " -цифра");
+                    }
+                }
+            }
+            string tmp = "";
+            tmp += text[0];
+            tmp += text[1];
+           
+            int tmpint = Convert.ToInt32(tmp);
+            if (tmpint % 2 == 0)
+            {
+                Console.WriteLine(tmpint + " - четное");
+            }
+            else Console.WriteLine(tmpint + " - нечетное");
+        }
+    }
+}
---
+++ minmax/nullornotnull/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nullornotnull
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] www = new string[2];
+            www[0] = "a b C";
+            www[1] = "d Q e";
+            Console.WriteLine(DecodeMessage(www));
+            
+           
+        }
+        private static string DecodeMessage(string[] lines)
+        {
+            
+
+            List<string> words = new List<string>();
+            for (int i = 0; i < lines.Length; i++)
+            {
+                string[] a = lines[i].Split(' ');
+                for (int j = 0; j < a.Length; j++) {
+                    if (string.IsNullOrEmpty(a[i])) continue;
+                    if (Char.IsUpper(a[j].ElementAt(0)))
+                    {
+                        words.Add(a[j]);
+                    }
+                }
+            }
+            words.Reverse();
+            return string.Join(" ", words.ToArray());
+        }
+    }
+}
