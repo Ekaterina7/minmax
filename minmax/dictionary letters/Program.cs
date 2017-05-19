480acf47ba92f484fa41f97342021f11ad52af3c
Ekaterina7 <mmmkatia@bk.ru>
2017-05-19 19:01:33 +0300

    add

---
+++ minmax/dictionary letters/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dictionary_letters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = System.IO.File.ReadAllText(@"C:\Users\Екатерина\Desktop\средний.txt");
+            var dictionary = new Dictionary<string, int>();
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (!dictionary.ContainsKey(Convert.ToString(text[i])))
+                {
+                    dictionary[Convert.ToString(text[i])] = 1;
+                }
+                else dictionary[Convert.ToString(text[i])]++;
+            }
+            foreach (var item in dictionary)
+            {
+                Console.WriteLine(item.Key + "\t" + item.Value);
+            }
+           
+        }
+    }
+}
---
+++ minmax/dictionary words/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dictionary_words
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = System.IO.File.ReadAllText(@"C:\Users\Екатерина\Desktop\средний.txt");
+            string[] textmas = text.Split(' ', '-', '.',',');
+
+            var dictionary = new Dictionary<string, int>();
+            for (int i = 0; i < textmas.Length; i++)
+            {
+                if(textmas[i]!="")
+                if (!dictionary.ContainsKey(Convert.ToString(textmas[i])))
+                {
+                    dictionary[Convert.ToString(textmas[i])] = 1;
+                }
+                else dictionary[Convert.ToString(textmas[i])]++;
+            }
+            foreach (var item in dictionary)
+            {
+                Console.WriteLine(item.Key + "\t" + item.Value);
+            }
+
+        }
+    }
+}
